0181ba73df0d45ad588b6b08339dd8ffb1508934
KEVIN <38305127+kkao99@users.noreply.github.com>
2018-12-26 23:01:41 -0500

    Add files via upload

---
+++ spine.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define spine int
+#define reconstruct main
+#define xs ()
+#define I_TRULY printf
+#define LOVE_MY ("")
+#define SPINE_RECONSTRUCT ;
+
+/*
+* show love to spine reconstruct ❤︎❤︎❤︎
+*/
+
+spine reconstruct xs {
+
+    I_TRULY LOVE_MY SPINE_RECONSTRUCT
+
+}
